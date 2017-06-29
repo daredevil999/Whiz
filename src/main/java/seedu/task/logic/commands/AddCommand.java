2291b78f1904aaee9eddf85c3eab9e2611a1b823
KIANMING-PC\KiANMiNG <kianming@live.com.sg>
2017-06-29 21:49:27 +0800

    Additional

--- src/main/java/seedu/task/logic/commands/AddCommand.java
+++ src/main/java/seedu/task/logic/commands/AddCommand.java
@@ -14,12 +14,12 @@ public abstract class AddCommand extends UndoableCommand {
     		+ "Adds a purchased stock to the stock manager.\n"
             + "The order of the parameters is flexible\n\n"
     		+ "Adding a newly purchased stock.\n"
-            + "Parameters: STOCK_NAME /c STOCKCODE /d DATEOFPURCHASE /l NUMOFSHARES /p PURCHASE PRICE \n"
+            + "Parameters: [STOCK_NAME || STOCKCODE] /d DATEOFPURCHASE /l NUMOFSHARES /p PURCHASE PRICE \n"
     		+ "DATEOFPURCHASE can be in words or MM-DD-YY\n"
             + "Example: " + COMMAND_WORD
             + " DBS GROUP HOLDINGS LTD /l 1000 /p 23.05 /d today\n\n"
-            + "Example with the use of Stock Code: \n" + COMMAND_WORD
-            + " /c D05 /l 1000 /p 23.05 /d today\n\n";
+            + "Example with the use of Stock Code of DBS: \n" + COMMAND_WORD
+            + " D05 /l 1000 /p 23.05 /d today\n\n";
     
     /**
      * Executes the command and returns the result message.
